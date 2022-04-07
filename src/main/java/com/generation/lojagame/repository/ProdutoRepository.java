9a7ae6972b1436e04944acf4677e2783b518c066
Viviane110199 <vivianerego22@gmail.com>
2022-04-06 21:12:11 -0300

    Atualizando

--- src/main/java/com/generation/lojagame/repository/ProdutoRepository.java
+++ src/main/java/com/generation/lojagame/repository/ProdutoRepository.java
@@ -6,9 +6,11 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.generation.lojagame.model.Produto;
 
+@Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long>{
 	
 	public List<Produto> findAllByNomeContainingIgnoreCase(String nome);
